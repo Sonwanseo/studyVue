e668e9f4c7493bc19aced1b6c9dc78d29562a719
Sonwanseo <sonwanseo@gmail.com>
2021-02-16 17:34:45 +0900

    Chapter 6

---
+++ Book/Chapter 6/single-file-component.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="hello">
+        {{msg}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'hello',
+    data() {
+        return {
+            msg: 'Vue.js 앱에 오신 걸 환영합니다.'
+        }
+    }
+}
+</script>
+
+<!-- CSS 범위 지정을 위해 해당 컴포넌트에만 "scoped 속성을 추가하세요. -->
+<style scoped>
+</style>
